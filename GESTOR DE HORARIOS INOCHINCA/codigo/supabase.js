b81eefd1b91b5964b1b3a35217314d0a26d7c513
aguilar123312 <aguilardaza123@gmail.com>
2026-09-14 15:28:18 -0500

    Update Supabase URL in supabase.js

--- GESTOR DE HORARIOS INOCHINCA/codigo/supabase.js
+++ GESTOR DE HORARIOS INOCHINCA/codigo/supabase.js
@@ -1,5 +1,5 @@
 // Configuración de Supabase
-const SUPABASE_URL = "https://TU_PROYECTO.supabase.co";
+const SUPABASE_URL = "https://crdlpylvxwcewfredjrw.supabase.co";
 const SUPABASE_ANON_KEY = "TU_LLAVE_ANON_PUBLIC";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
