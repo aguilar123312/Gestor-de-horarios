5eb65a40c45724f2ca595491bd3a331c613ccf06
aguilar123312 <aguilardaza123@gmail.com>
2026-09-14 15:13:57 -0500

    Add Supabase configuration in supabase.js

---
+++ GESTOR DE HORARIOS INOCHINCA/codigo/supabase.js
@@ -0,0 +1,5 @@
+// Configuración de Supabase
+const SUPABASE_URL = "https://TU_PROYECTO.supabase.co";
+const SUPABASE_ANON_KEY = "TU_LLAVE_ANON_PUBLIC";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
